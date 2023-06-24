99ecc6bfa88416316c7a734d7ed36ad1914431b5
Goutham Rajesh <gouthamkannus10@gmail.com>
2023-06-24 20:06:41 +0530

    Changed Stable.php

--- course-file-pages/stable.php
+++ course-file-pages/stable.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Course File</title>
+    <title>Department Course File</title>
     <link rel="stylesheet" href="../css/course-file.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -13,6 +13,60 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <style>
+    .breadcrumb{
+        display: inline-block;
+        padding: 0;
+        margin: 0;
+        border-radius: 5px 25px 25px 5px;
+        overflow: hidden;
+    }
+    .breadcrumb li{
+        float: left;
+        list-style-type: none;
+        margin-right: 3px;
+        position: relative;
+        z-index: 1;
+    }
+    .breadcrumb li:before{ display: none; }
+    .breadcrumb li:after{
+        content: "";
+        width: 40px;
+        height: 100%;
+        background: #428dff;
+        position: absolute;
+        top: 0;
+        right: -20px;
+        z-index: -1;
+    }
+    .breadcrumb li:nth-last-child(2):after,
+    .breadcrumb li:last-child:after{ display: none; }
+    .breadcrumb li a,
+    .breadcrumb li:last-child{
+        display: block;
+        padding: 8px 15px;
+        font-size: 14px;
+        font-weight: bold;
+        color: #fff;
+        border-radius: 0 25px 25px 0;
+        box-shadow: 5px 0 5px -5px #333;
+    }
+    .breadcrumb li a{ background: #428dff; }
+    .breadcrumb li:last-child{
+        background: #ebf3fe;
+        color: #428dff;
+        margin-right: 0;
+    }
+    @media only screen and (max-width: 479px){
+        .breadcrumb li a,
+        .breadcrumb li:last-child{ padding: 8px 10px; }
+    }
+    @media only screen and (max-width: 359px){
+        .breadcrumb li a,
+        .breadcrumb li:last-child{ padding: 8px 7px; }
+    }
+    </style>
 
 <?php 
     session_start();
@@ -35,10 +89,20 @@
     $_SESSION['subjectCode'] = $subjectCode;
 
 ?>
+
+
 </head>
 <body>
+
+    <div class="demo">
+        <ol class="breadcrumb">
+            <li><a href="../course-file.php?subject=<?php echo urlencode($subjectCode); ?>" style="text-decoration: none;">Course File</a></li>
+            <li class="active">Pick Your Option</li>
+        </ol>
+    </div>
+
     <div class="fac_name">
-        <h1>Course File</h1>
+        <h1>Department Course File</h1>
         Faculty Name: <?php echo $current_user; ?>
         <br>
         Subject Code: <?php echo $subjectCode; ?>
